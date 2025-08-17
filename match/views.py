a29fa9ee3d0cb8f2b3b81f78051ea4546d179108
daaaaain <dainkim6280@gmail.com>
2025-08-17 17:42:30 +0900

    임시 저장

--- match/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, Django! 여기는 match 앱입니다.")
-
-# Create your views here.
